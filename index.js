e8b6a62db3f9985aeb5f5a97223c1d19d35b391e
Charlike Mike Reagent <opensource@tunnckocore.com>
2019-10-13 07:24:19 +0300

    feat: more better support
    Signed-off-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- index.js
+++ index.js
@@ -1,52 +1,77 @@
 const { readFileSync } = require('fs');
-const { parse } = require('url');
+const url = require('url');
 const { send } = require('micro');
-const isObject = require('isobject');
+const path = require('path');
 const redirect = require('micro-redirect');
 const getRequestsOnly = require('micro-get');
-const getPkg = require('get-pkg');
+const packageJson = require('@tunnckocore/package-json');
 const parseGithub = require('parse-github-url');
 
+// eslint-disable-next-line max-statements
 module.exports = getRequestsOnly(async (req, res) => {
-  const { pathname } = parse(req.url);
+  // eslint-disable-next-line node/no-deprecated-api
+  const parsed = url.parse(req.url);
 
-  if (pathname === '/') {
+  if (parsed.pathname === '/') {
     return readFileSync('./home.html', 'utf8');
   }
 
-  const name = pathname.slice(1);
+  const parts = clean(parsed.pathname).split('/');
   let pkg = null;
+
+  // @tunnckocore/package-json
+  // @tunnckocore/package-json/<branch>
+  // @tunnckocore/package-json@<tag|version>
+  // @tunnckocore/package-json@<tag|version>/<branch>
+
+  // get-pkg
+  // get-pkg/<branch>
+  // get-pkg@<tag|version>
+  // get-pkg@<tag|version>/<branch>
+
+  const name = parts.length >= 2 ? parts.slice(0, 2).join('/') : parts[0];
+  const isScoped = name.startsWith('@');
 
   try {
     pkg = await packageJson(name);
-  } catch (e) {
-    return redirect(res, 301, '/');
+  } catch (err) {
+    console.error(err);
+    return send(res, 404, `Package not found or loading error ${name}`);
   }
 
-  let url = pkg.repository || (pkg.bugs && pkg.bugs.url);
+  const repoBranch = isScoped ? parts.slice(2) : parts.slice(1);
+  const repository = (pkg.repository && pkg.repository.url) || pkg.repository;
+  const directory = (pkg.repository && pkg.repository.directory) || '';
+  const bugs = (pkg.bugs && pkg.bugs.url) || pkg.bugs;
+  const repoBr = repoBranch.length > 0 ? repoBranch : ['master'];
 
-  if (isObject(url)) {
-    url = url.url; // eslint-disable-line
+  const branch = path.join(...repoBr);
+
+  const dir = directory ? `/tree/${branch}/${clean(directory)}` : '';
+
+  const pkgUrl = clean(repository ? repository.replace('.git', '') : bugs);
+
+  if (pkgUrl) {
+    const gh = parseGithub(`${pkgUrl}${dir}`);
+
+    if (dir) {
+      return redirect(res, 302, `https://${gh.host}/${gh.repo}${dir}`);
+    }
+
+    const pathname = gh.pathname.replace('.git', '');
+    return redirect(res, 302, `https://${gh.host}/${pathname}`);
   }
-  if (!isString(url)) {
-    return send(res, 404, 'Some problem appeared');
+  if (isString(pkg.homepage)) {
+    return redirect(res, 302, pkg.homepage);
   }
 
-  const { repo } = parseGithub(url);
-
-  // TODO: Support for GitLab, BitBucket, etc? - Send a pull request.
-  return redirect(res, 301, `https://github.com/${repo}`);
+  return redirect(res, 302, `https://npmjs.com/package/${name}`);
 });
 
-function packageJson (name) {
-  return new Promise((resolve, reject) => {
-    getPkg(name, (er, pkg) => {
-      if (er) return reject(er);
-      resolve(pkg);
-    });
-  });
+function isString(val) {
+  return val && typeof val === 'string';
 }
 
-function isString (url) {
-  return url && typeof url === 'string';
+function clean(val) {
+  return isString(val) ? val.replace(/^\/|\/$/, '') : val;
 }
